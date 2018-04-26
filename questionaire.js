400d17f7c77180be955b069276cfc8262dd1886d
adassor <adassor@sfu.ca>
2018-04-26 14:09:26 -0700

    added trials and initial validation

--- questionaire.js
+++ questionaire.js
@@ -12,12 +12,12 @@ function disable_next(){
 
 var contact_info = {
     type: 'survey-text',
-    preamble: header + "<h3>Participant Contact Information</h3>",
+    preamble: header + "<h3>Contact Information</h3>",
     questions: [
-        {prompt: "Surname", required: true},
-        {prompt: "First Name", required: true},
-        {prompt: "Phone Number", required: true},
-        {prompt: "Email", required: true}
+        {prompt: "Surname"},
+        {prompt: "First Name"},
+        {prompt: "Phone Number"},
+        {prompt: "Email"}
     ],
     data: {
         subject_id
@@ -29,20 +29,62 @@ var contact_info = {
     },
     on_finish: function(contact_info){
         form_curr.removeEventListener("change", validate_contact_info);
+    }
+}
+var personal_info = {
+    type: 'survey-text',
+    preamble: header + "<h3>Personal Information</h3>",
+    questions: [
+        {prompt: "Age"},
+        {prompt: "Gender"},
+        {prompt: "Birth Country"},
+        {prompt: "If you have any hearing problems, please specify"},
+        {prompt: "If you have any language disorders, please specify"},
+        {prompt: "If you have any learning disorders, please specify"}
+    ],
+    data: {
+        subject_id
     },
-    on_finish: function(data){
-        data.blur_count = blur_count;
+    on_load: function(contact_info){
+        disable_next();
+        form_curr = document.getElementById("jspsych-content");
+        form_curr.addEventListener("change", validate_personal_info);
+    },
+    on_finish: function(contact_info){
+        form_curr.removeEventListener("change", validate_personal_info);
+    }
+}
+var background_info = {
+    type: 'survey-text',
+    preamble: header + "<h3>Background Information</h3>",
+    questions: [
+        {prompt: "Age of Arrival in Canada"},
+        {prompt: "Native Language"},
+        {prompt: "Parent 1's Native Language"},
+        {prompt: "Parent 2's Native Language"}
+    ],
+    data: {
+        subject_id
+    },
+    on_load: function(contact_info){
+        disable_next();
+        form_curr = document.getElementById("jspsych-content");
+        form_curr.addEventListener("change", validate_background_info);
+    },
+    on_finish: function(contact_info){
+        form_curr.removeEventListener("change", validate_background_info);
     }
 }
 var language_info = {
     type: 'survey-text',
-    preamble: header + "<p>Any explanatory text required to define these terms.</p>",
+    preamble: header + "<h3>Language Information</h3>",
     questions: [
         {prompt: "Birth Country"},
         {prompt: "Age of Arrival in Canada"},
         {prompt: "Native Language"},
         {prompt: "Parent 1's Native Language"},
         {prompt: "Parent 2's Native Language"},
+        
     ],
     data: {
         subject_id
@@ -54,13 +96,31 @@ var language_info = {
     },
     on_finish: function(language_info){
         form_curr.removeEventListener("change", validate_language_info);
+    }
+}
+var musical_info = {
+    type: 'survey-text',
+    preamble: header + "<h3>Musical Background</h3>",
+    questions: [
+        {prompt: "Have you played any musical instruments? If so, please specify which one(s) you have played, the age you began and the number of years you have practiced."}
+    ],
+    data: {
+        subject_id
+    },
+    on_load: function(musical_info){
+        disable_next();
+        form_curr = document.getElementById("jspsych-content");
+        form_curr.addEventListener("change", validate_musical_info);
+    },
+    on_finish: function(language_info){
+        form_curr.removeEventListener("change", validate_musical_info);
     },
     on_finish: function(data){
         data.blur_count = blur_count;
     }
 }
 jsPsych.init({
-    timeline: [contact_info, language_info],
+    timeline: [contact_info, personal_info, background_info, language_info, musical_info],
     show_progress_bar: true,
     //Checks how many times user left
     on_interaction_data_update: function(data){
--- validators.js
+++ validators.js
@@ -1,18 +1,26 @@
-//regex from: https://cs.chromium.org/chromium/src/third_party/WebKit/LayoutTests/fast/forms/resources/ValidityState-typeMismatch-email.js?sq=package:chromium&type=cs
-var email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+var next_button;
+var form;
+var questions;
+var is_valid;
+
+function prepare_globals(){
+    next_button = document.getElementById("jspsych-survey-text-next");
+    form = document.getElementById("jspsych-content");
+    questions = form.children;
+    is_valid = true;
+}
 
 function validate_contact_info(){
-    var next_button = document.getElementById("jspsych-survey-text-next");
-    var form = document.getElementById("jspsych-content");
+    prepare_globals();
     next_button.disabled = true;
-    var questions = form.children;
-    var is_valid = true;
 
     lname = form.children[1].children[1].value
     fname = form.children[2].children[1].value
-    // age   = form.children[3].children[1].value
-    phone = form.children[4].children[1].value
-    email = form.children[5].children[1].value
+    phone = form.children[3].children[1].value
+    email = form.children[4].children[1].value
+
+    //regex from: https://cs.chromium.org/chromium/src/third_party/WebKit/LayoutTests/fast/forms/resources/ValidityState-typeMismatch-email.js?sq=package:chromium&type=cs
+    var email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if(is_valid && (lname.length == 0 || lname.length > 100)){
         next_button.innerText = "Invalid surname";
@@ -41,17 +49,25 @@ function validate_contact_info(){
     }
 }
 
-function validate_language_info(){
-    var next_button = document.getElementById("jspsych-survey-text-next");
-    var form = document.getElementById("jspsych-content");
+function validate_personal_info(){
+    prepare_globals();
     next_button.disabled = true;
-    console.log("attempting validation");
-    var questions = form.children;
-    var is_valid = true;
+}
 
+function validate_background_info(){
+    prepare_globals();
+    next_button.disabled = true;
+}
+function validate_language_info(){
+    prepare_globals();
+    next_button.disabled = true;
 
     if(is_valid){
         next_button.disabled = false;
         next_button.innerText = "Continue";
     }
+}
+function validate_musical_info(){
+    prepare_globals();
+    next_button.disabled = true;
 }
