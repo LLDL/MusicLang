1e911dbe79d30875f0edee1e77649194f6824e93
Ankit Dassor <adassor@sfu.ca>
2018-04-26 08:48:32 -0700

    Dependencies, README

---
+++ jatos-3.2.1/loader.bat
@@ -0,0 +1,200 @@
+@REM JATOS loader for Windows
+@setlocal enabledelayedexpansion
+
+@echo off
+
+rem # Change IP address and port here
+set address=127.0.0.1
+set port=9000
+
+rem # Don't change after here unless you know what you're doing
+rem ###################################
+
+set JATOS_HOME=%~dp0
+set JATOS_HOME=%JATOS_HOME:~0,-1%
+set LOCAL_JRE=jre\win64_jre
+
+rem # Detect if we were double clicked, although theoretically A user could
+rem # manually run cmd /c
+for %%x in (!cmdcmdline!) do if %%~x==/c set DOUBLECLICKED=1
+
+if _%DOUBLECLICKED%_==_1_ (
+  call :start
+  exit
+)
+
+rem # If we were started from CMD, evaluate start parameter
+if "%1"=="start" (
+  call :start %*
+  exit /b
+) else if "%1"=="stop" (
+  call :stop
+  exit /b
+) else (
+  @echo "Usage: loader.bat start|stop"
+  exit /b
+)
+
+exit /b
+
+rem ### Functions ###
+
+:start
+  rem # Check if JATOS is already running
+  wmic process where "name like '%%java%%'" get commandline | findstr /i /c:"jatos" > NUL && (
+    echo JATOS already running
+    if _%DOUBLECLICKED%_==_1_ pause
+    goto:eof
+  )
+  rem # If there is an old RUNNING_PID file of an earlier JATOS run that wasn't orderly closed delete it
+  IF EXIST "%JATOS_HOME%\RUNNING_PID" (
+    del "%JATOS_HOME%\RUNNING_PID"
+  )
+
+  echo Starting JATOS ... please wait
+
+  rem # We use the value of the JAVA_OPTS environment variable if defined, rather than the config.
+  set _JAVA_OPTS=%JAVA_OPTS%
+  if "!_JAVA_OPTS!"=="" set _JAVA_OPTS=!CFG_OPTS!
+
+  rem # We keep in _JAVA_PARAMS all -J-prefixed and -D-prefixed arguments
+  rem # "-J" is stripped, "-D" is left as is, and everything is appended to JAVA_OPTS
+  set _JAVA_PARAMS=
+  call :getparams %*
+
+  rem # Generate application secret for the Play framework
+  rem # If it's the first start, create a new secret, otherwise load it from the file.
+  IF NOT EXIST "%JATOS_HOME%\play.crypto.secret" (
+    set rand=%RANDOM%%RANDOM%%RANDOM%
+    echo !rand!>"%JATOS_HOME%\play.crypto.secret"
+  )
+  set /p SECRET=<"%JATOS_HOME%\play.crypto.secret"
+  
+  call :checkjava
+  if errorlevel 1 (
+    exit /b 1
+  )
+
+  rem # Start JATOS with configuration file and application secret
+  set JATOS_OPTS=-Dconfig.file="conf/production.conf" -Dplay.crypto.secret=!SECRET! -Dhttp.port=%port% -Dhttp.address=%address%
+  if _%DOUBLECLICKED%_==_1_ (
+    set JATOS_OPTS=-Dpidfile.path="NUL" %JATOS_OPTS%
+  ) else (
+    set JATOS_OPTS=-Dpidfile.path="%JATOS_HOME%\RUNNING_PID" %JATOS_OPTS%
+  )
+
+  set "APP_CLASSPATH=%JATOS_HOME%\lib\*"
+  set "APP_MAIN_CLASS=play.core.server.NettyServer"
+  set CMD="%JAVACMD%" %JATOS_OPTS% !_JAVA_OPTS! -cp "%APP_CLASSPATH%" %APP_MAIN_CLASS%
+  cd %JATOS_HOME%
+  start /b call %CMD% > nul
+  
+  echo To use JATOS type %address%:%port% in your browser's address bar
+  goto:eof
+
+:stop
+  if not exist "%JATOS_HOME%\RUNNING_PID" (
+    echo This JATOS was not running
+    goto:eof
+  )
+  echo Stopping JATOS
+  set /p PID=<"%JATOS_HOME%\RUNNING_PID"
+  taskkill /pid %PID% /f
+  if errorlevel 1 (
+    del "%JATOS_HOME%\RUNNING_PID"
+	echo ...failed
+  ) else (
+    del "%JATOS_HOME%\RUNNING_PID"
+    echo ...stopped
+  )
+  goto:eof
+  
+:checkjava
+  rem # Java's path can be defined in PATH or JAVA_HOME
+  rem # Don't confuse JAVA_HOME with JATOS_HOME
+  if exist "%JATOS_HOME%\%LOCAL_JRE%" (
+    set "JAVA_HOME=%JATOS_HOME%\%LOCAL_JRE%"
+	echo JATOS uses local JRE
+  )
+  if not "%JAVA_HOME%"=="" (
+    if exist "%JAVA_HOME%\bin\java.exe" (
+      set "JAVACMD=%JAVA_HOME%\bin\java.exe"
+    )
+  )
+  
+  if "%JAVACMD%"=="" set JAVACMD=java
+
+  rem # Detect if this java is ok to use.
+  for /F %%j in ('"%JAVACMD%" -version  2^>^&1') do (
+    if %%~j==Java set JAVAINSTALLED=1
+    if %%~j==openjdk set JAVAINSTALLED=1
+  )
+  
+  if "%JAVAINSTALLED%"=="" (
+    echo.
+    echo A Java JDK or JRE is not installed or cannot be found.
+    echo.
+    echo Please go to
+    echo   http://www.oracle.com/technetwork/java/javase/downloads/index.html
+    echo and download a valid Java JRE and install it before running JATOS.
+    echo.
+    echo If you think this message is in error, please check
+    echo your environment variables to see if "java.exe" is
+    echo available via JAVA_HOME or PATH.
+    echo.
+
+    if _%DOUBLECLICKED%_==_1_ pause
+    exit /b 1
+  )
+  exit /b 0
+
+:getparams
+  :param_loop
+  call set _PARAM1=%%1
+  set "_TEST_PARAM=%~1"
+  if ["!_PARAM1!"]==[""] goto param_afterloop
+
+  rem # ignore arguments that do not start with '-'
+  if "%_TEST_PARAM:~0,1%"=="-" goto param_java_check
+  shift
+  goto param_loop
+
+  :param_java_check
+  if "!_TEST_PARAM:~0,2!"=="-J" (
+    rem # strip -J prefix
+    set _JAVA_PARAMS=!_JAVA_PARAMS! !_TEST_PARAM:~2!
+    shift
+    goto param_loop
+  )
+
+  if "!_TEST_PARAM:~0,2!"=="-D" (
+    rem # test if this was double-quoted property "-Dprop=42"
+    for /F "delims== tokens=1,*" %%G in ("!_TEST_PARAM!") DO (
+      if not ["%%H"] == [""] (
+        set _JAVA_PARAMS=!_JAVA_PARAMS! !_PARAM1!
+      ) else if [%2] neq [] (
+        rem # it was a normal property: -Dprop=42 or -Drop="42"
+        call set _PARAM1=%%1=%%2
+        set _JAVA_PARAMS=!_JAVA_PARAMS! !_PARAM1!
+        rem # Overwrite global variables address and port if we have them
+        if "%1%" == "-Dhttp.port" (
+          set port=%2%
+        ) else if "%1%" == "-Dhttp.address" (
+          set address=%2%
+        )
+        shift
+      )
+    )
+  ) else (
+    if "!_TEST_PARAM!"=="-main" (
+      call set CUSTOM_MAIN_CLASS=%%2
+      shift
+    )
+  )
+  shift
+  goto param_loop
+
+  :param_afterloop
+  set _JAVA_OPTS=!_JAVA_OPTS! !_JAVA_PARAMS!
+  exit /b 0
+
---
+++ jatos-3.2.1/loader.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+# JATOS loader for Linux and MacOS X
+
+# Change IP address and port here
+# Alternatively you can use command-line arguments -Dhttp.address and -Dhttp.port
+address="127.0.0.1"
+port="9000"
+
+# Don't change after here unless you know what you're doing
+###########################################################
+
+# Get JATOS directory
+dir="$( cd "$( dirname "$0" )" && pwd )"
+pidfile="$dir/RUNNING_PID"
+args=${@:2}
+
+function start() {
+	checkAlreadyRunning
+	checkJava
+
+	echo -n "Starting JATOS"
+
+	# Generate application secret for the Play framework
+	secret="$(LC_ALL=C tr -cd '[:alnum:]' < /dev/urandom 2>/dev/null | dd bs=64 count=1 2>/dev/null)"
+
+	if [ ! -f "$dir/bin/jatos" ]
+	then
+		echo -e "\n$dir/bin/jatos doesn't exist!"
+		exit 1
+	fi
+
+	# In case './bin/jatos' isn't executable set the x bit
+	chmod u+x "$dir/bin/jatos"
+
+	# Start JATOS with configuration file and application secret
+	"$dir/bin/jatos" -Dconfig.file="$dir/conf/production.conf" -Dplay.crypto.secret=$secret -Dhttp.port=$port -Dhttp.address=$address -J-server $args > /dev/null &
+
+	echo "...started"
+	printAddressAndPort
+}
+
+function stop() {
+	if [ ! -f "$pidfile" ] || ! kill -0 $(cat "$pidfile") 2>&1 >/dev/null
+	then
+		echo "This JATOS was not running"
+		exit 1
+	fi
+	echo -n "Stopping JATOS"
+	kill -SIGTERM $(cat "$pidfile")
+	rm -f "$pidfile"
+	echo "...stopped"
+}
+
+function checkAlreadyRunning() {
+	if [ -f "$pidfile" ] && kill -0 $(cat "$pidfile") 2>&1 >/dev/null
+	then
+		echo "There seems to be a running JATOS."
+		exit 1
+	fi
+	# Delete old PID file (just to be sure)
+	rm -f "$pidfile"
+
+	if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null
+	then
+		echo "Some other program is using port $port already. Maybe another JATOS?"
+		exit 1
+	fi
+}
+
+function checkJava() {
+	# Get Java version if installed
+	if type -p java
+	then
+		echo "Found Java"
+		java_version=$(java -version 2>&1 | sed 's/.*version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+	elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]
+	then
+		echo "Found Java in JAVA_HOME"
+		java_version=$($JAVA_HOME/bin/java -version 2>&1 | sed 's/.*version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+	fi
+
+	# If we don't have a version or if the version is not a number or if the version is smaller 18 (Java 8) try to find a local Java installation
+	if [[ -z "$java_version" ]] || [[ ! "$java_version" =~ ^[0-9]+$ ]] || [[ "$java_version" -lt 18 ]]
+	then
+		if [[ -n "$dir/jre/linux_x64_jre" ]] && [[ -x "$dir/jre/linux_x64_jre/bin/java" ]]
+		then
+			echo "Jatos uses local JRE"
+			export JAVA_HOME="$dir/jre/linux_x64_jre"
+		elif [[ -n "$dir/jre/mac_x64_jre" ]] && [[ -x "$dir/jre/mac_x64_jre/bin/java" ]]
+		then
+			echo "Jatos uses local JRE"
+			export JAVA_HOME="$dir/jre/mac_x64_jre"
+		else
+			echo "No Java found"
+			exit 1
+		fi
+	fi
+}
+
+function printAddressAndPort() {
+	usedAddress=$address
+	usedPort=$port
+
+	for i in $args ; do
+		case "$i" in
+		-Dhttp.address=*)
+			array=(${i//=/ })
+			usedAddress=${array[1]}
+			;;
+		-Dhttp.port=*)
+			array=(${i//=/ })
+			usedPort=${array[1]}
+			;;
+		esac
+	done
+
+	echo "To use JATOS type $usedAddress:$usedPort in your browser's address bar"
+}
+
+case "$1" in
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;
+	restart)
+		stop
+		# Check that JATOS' port is free
+		while lsof -Pi :$port -sTCP:LISTEN -t >/dev/null
+		do
+			sleep 1
+		done
+		start
+		;;
+	*)
+		echo "Usage: loader.sh start|stop|restart"
+		exit 1
+		;;
+esac
