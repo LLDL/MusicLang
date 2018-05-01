4a46a4035d1a62ab134fe6b6418ec6aa6efc3597
adassor <adassor@sfu.ca>
2018-05-01 09:39:18 -0700

    made common function calls easier

--- validators.js
+++ validators.js
@@ -10,7 +10,7 @@ function set_globals(){
     is_valid = true;
 }
 
-function allow_next(enable){
+function allow_next(enable, text_on_false = "Please answer all the questions above"){
     next_button = document.getElementById("jspsych-survey-text-next");
     if(enable){
         next_button.disabled = false;
@@ -18,39 +18,43 @@ function allow_next(enable){
         next_button.innerText = "Continue";
     }
     if(!enable){
-        next_button.disabled = false;  
+        next_button.disabled = true;  
         next_button.style.backgroundColor = "#903738";
-        next_button.innerText = "Please answer all the questions above";
+        next_button.innerText = text_on_false;
     }
+}
+
+function get_answer(question){
+    form = document.getElementById("jspsych-content");
+    return form.children[question].children[1].value;
 }
 
 function validate_contact_info(){
     set_globals();
     allow_next(false);
 
-    lname = form.children[1].children[1].value;
-    fname = form.children[2].children[1].value;
-    phone = form.children[3].children[1].value;
-    email = form.children[4].children[1].value;
+    var lname = get_answer(1);
+    var fname = get_answer(2);
+    var phone = get_answer(3);
+    var email = get_answer(4);
 
     //regex from: https://cs.chromium.org/chromium/src/third_party/WebKit/LayoutTests/fast/forms/resources/ValidityState-typeMismatch-email.js?sq=package:chromium&type=cs
     var email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if(is_valid && (lname.length == 0 || lname.length > 100)){
-        next_button.innerText = "Invalid surname";
+        allow_next(false, "Invalid surname");
         is_valid = false;
     } 
     if(is_valid && fname.length == 0 || lname.length > 100){
-        next_button.innerText = "Invalid first name";
+        allow_next(false, "Invalid first name");
         is_valid = false;
     }
     if(is_valid && phone.length < 5 || phone.length > 100){
-        next_button.innerText = "Invalid phone number";
+        allow_next(false, "Invalid phone number");
         is_valid = false;
     } 
     if(is_valid && !email_regex.test(String(email).toLowerCase())){
-        
-        next_button.innerText = "Invalid email address";
+        allow_next(false, "Invalid email address");
         is_valid = false;
     }
     if(is_valid){
@@ -62,12 +66,12 @@ function validate_personal_info(){
     set_globals();
     allow_next(false);
 
-    age = form.children[1].children[1].value;
-    gender = form.children[2].children[1].value;
-    birth_country = form.children[3].children[1].value;
-    hearing_problems = form.children[4].children[1].value;
-    language_disorders = form.children[5].children[1].value;
-    learning_disorders = form.children[6].children[1].value;
+    var age = get_answer(1);
+    var gender = get_answer(2);
+    var birth_country = get_answer(3);
+    var hearing_problems = get_answer(4);
+    var language_disorders = get_answer(5);
+    var learning_disorders = get_answer(6);
 
     if(is_valid && (isNaN(age) || age <= 0 || age > 120)){
         next_button.innerText = "Invalid age";
@@ -101,10 +105,10 @@ function validate_personal_info(){
 function validate_background_info(){
     set_globals();
     allow_next(false);
-    age_arrival = form.children[1].children[1].value;
-    native_language = form.children[2].children[1].value;
-    parent_1_lang = form.children[3].children[1].value;
-    parent_2_lang = form.children[4].children[1].value;
+    var age_arrival = get_answer(1);
+    var native_language = get_answer(2);
+    var parent_1_lang = get_answer(3);
+    var parent_2_lang = get_answer(4);
     
     if(is_valid && (isNaN(age) || age < 0 || age > 120)){
         next_button.innerText = "Invalid age of arrival in Canada";
@@ -130,7 +134,7 @@ function validate_language_info(){
     set_globals();
     allow_next(false);
     
-    dom_lang1 = form.children[1].children[1].value;
+    var dom_lang1 = get_answer(1);
 
     if (dom_lang1.length < 2 || dom_lang1.length > 50){
         next_button.innerText = "Please enter at least your most dominant language";
@@ -155,7 +159,7 @@ function validate_musical_info(){
     set_globals();
     allow_next(false);
 
-    musical_instruments = form.children[1].children[1].value;
+    var musical_instruments = get_answer(1);
 
     if(is_valid && (musical_instruments.length == 0 || musical_instruments.length > 1000)){
         next_button.innerText = "Please enter your musical experience or n/a";
