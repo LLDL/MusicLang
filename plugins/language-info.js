320a479349414bc19c17de725eefbbf45f633f5f
adassor <adassor@sfu.ca>
2018-05-09 16:15:58 -0700

    beautify and refactor code

--- plugins/language-info.js
+++ plugins/language-info.js
@@ -6,99 +6,91 @@
  * Ankit Dassor
  */
 
-jsPsych.plugins["language-info"] = (function() {
+jsPsych.plugins["language-info"] = (function () {
+	var plugin = {};
+	plugin.info = {
+		name: "language-info",
+		description: '',
+		parameters: {
+			languages: {
+				type: jsPsych.plugins.parameterType.STRING,
+				array: true,
+				pretty_name: 'Languages',
+				description: 'The languages the subject specified in language_info, ordered from most to least dominant',
+				default: undefined
+			},
+			preamble: {
+				type: jsPsych.plugins.parameterType.STRING,
+				pretty_name: 'Preamble',
+				default: null,
+				description: 'HTML formatted string to display at the top of the page above all the languages.'
+			},
+			button_label: {
+				type: jsPsych.plugins.parameterType.STRING,
+				pretty_name: 'Button label',
+				default: 'Continue',
+				description: 'The text that appears on the button to finish the trial.'
+			}
+		}
+	}
 
-  var plugin = {};
+	plugin.trial = function (display_element, trial) {
+		// Initialize form and add preamble if any
+		var html = '';
+		if (trial.preamble !== null) {
+			html += '<div id="jspsych-language-info-preamble" class="jspsych-language-info-preamble">' + trial.preamble + '</div>';
+		}
+		// Table headers and formatting
+		html += '<table align="center" id="jspsych-language-info-table class="jspsych-language-info-table">';
+		html += '<tr class="jspsych-language-info-header" id="jspsych-language-info-table-header">';
+		html += '<td class="jspsych-language-info-header-col">Language</td>';
+		html += '<td class="jspsych-language-info-header-col">Age Began</td>';
+		html += '<td class="jspsych-language-info-header-col">Years Learned</td>';
+		html += '</tr>';
 
-  plugin.info = {
-    name: "language-info",
-    description: '',
-    parameters: {
-      languages: {
-        type: jsPsych.plugins.parameterType.STRING,
-        array: true,
-        pretty_name: 'Languages',
-        description: 'The languages the subject specified in language_info, ordered from most to least dominant',
-        default: undefined
-      },
-      preamble: {
-        type: jsPsych.plugins.parameterType.STRING,
-        pretty_name: 'Preamble',
-        default: null,
-        description: 'HTML formatted string to display at the top of the page above all the languages.'
-      },
-      button_label: {
-        type: jsPsych.plugins.parameterType.STRING,
-        pretty_name: 'Button label',
-        default:  'Continue',
-        description: 'The text that appears on the button to finish the trial.'
-      }
-    }
-  }
+		//Show all languages in table
+		for (var i = 0; i < 4; i++) {
+			if (trial.languages[i] == undefined) {
+				break;
+			}
+			html += '<tr class="jspsych-language-info-lang-row" id="lang-row-' + i + '">';
+			html += '<td class="jspsych-language-info-lang">' + trial.languages[i] + '</td>';
+			html += '<td class="jspsych-language-info-age">' + '<input class="jspsych-language-info-age-response" type="text" name="#jspsych-language-info-age-response" size="10"></input>' + '</td>';
+			html += '<td class="jspsych-language-info-years">' + '<input class="jspsych-language-info-year-response" type="text" name="#jspsych-language-info-year-response" size="10"></input>' + '</td>';
+			html += '</tr>'
+		}
+		html += '</table>';
+		html += '<button id="jspsych-language-info-next" class="jspsych-language-info-next">' + trial.button_label + '</button>';
 
-  plugin.trial = function(display_element, trial) {
-    // Initialize form and add preamble if any
-    var html = '';
-    if(trial.preamble !== null){
-      html += '<div id="jspsych-language-info-preamble" class="jspsych-language-info-preamble">'+trial.preamble+'</div>';
-    }
-    // Table headers and formatting
-    html += '<table align="center" id="jspsych-language-info-table class="jspsych-language-info-table">';
-    html += '<tr class="jspsych-language-info-header" id="jspsych-language-info-table-header">';
-    html += '<td class="jspsych-language-info-header-col">Language</td>';
-    html += '<td class="jspsych-language-info-header-col">Age Began</td>';
-    html += '<td class="jspsych-language-info-header-col">Years Learned</td>';
-    html += '</tr>';
+		display_element.innerHTML = html;
+		display_element.querySelector('#jspsych-language-info-next').addEventListener('click', function () {
+			// measure response time
+			var endTime = (new Date()).getTime();
+			var response_time = endTime - startTime;
 
-    //Show all languages in table
-    for (var i = 0; i < 4; i++){
-      if(trial.languages[i] == undefined){
-        break;
-      }
-      html += '<tr class="jspsych-language-info-lang-row" id="lang-row-'+ i +'">';
-      html += '<td class="jspsych-language-info-lang">' + trial.languages[i] + '</td>';
-      html += '<td class="jspsych-language-info-age">' + '<input class="jspsych-language-info-age-response" type="text" name="#jspsych-language-info-age-response" size="10"></input>' + '</td>';
-      html += '<td class="jspsych-language-info-years">' + '<input class="jspsych-language-info-year-response" type="text" name="#jspsych-language-info-year-response" size="10"></input>' + '</td>';
-      html += '</tr>'
-    }
-    html += '</table>';
-    html += '<button id="jspsych-language-info-next" class="jspsych-language-info-next">'+trial.button_label+'</button>';
-
-    display_element.innerHTML = html;
-
-    
-    display_element.querySelector('#jspsych-language-info-next').addEventListener('click', function() {
-      // measure response time
-      var endTime = (new Date()).getTime();
-      var response_time = endTime - startTime;
-
-      // create object to hold responses
-      var lang_data = {};
-      var langs = display_element.querySelectorAll('.jspsych-language-info-lang');
-      var ages =  display_element.querySelectorAll('.jspsych-language-info-age-response');
-      var years = display_element.querySelectorAll('.jspsych-language-info-year-response');
-      for(var i = 0; i < langs.length; i++){
-        var id = langs[i].innerHTML;
-        var age = ages[i].value;
-        var year = years[i].value;
-        var obje = {};
-        obje[id] = [age, year];
-        Object.assign(lang_data, obje);
-      }
-      // save data
-      var trialdata = {
-        "rt": response_time,
-        "responses": JSON.stringify(lang_data)
-      };
-
-      display_element.innerHTML = '';
-
-      // next trial
-      jsPsych.finishTrial(trialdata);
-    });
-
-    var startTime = (new Date()).getTime();
-  };
-
-  return plugin;
+			// create object to hold responses
+			var lang_data = {};
+			var langs = display_element.querySelectorAll('.jspsych-language-info-lang');
+			var ages = display_element.querySelectorAll('.jspsych-language-info-age-response');
+			var years = display_element.querySelectorAll('.jspsych-language-info-year-response');
+			for (var i = 0; i < langs.length; i++) {
+				var id = langs[i].innerHTML;
+				var age = ages[i].value;
+				var year = years[i].value;
+				var obje = {};
+				obje[id] = [age, year];
+				Object.assign(lang_data, obje);
+			}
+			// save data
+			var trialdata = {
+				"rt": response_time,
+				"responses": JSON.stringify(lang_data)
+			};
+			display_element.innerHTML = '';
+			// next trial
+			jsPsych.finishTrial(trialdata);
+		});
+		var startTime = (new Date()).getTime();
+	};
+	return plugin;
 })();
--- validators.js
+++ validators.js
@@ -3,45 +3,47 @@ var langs;
 var color_enabled = "#37904b";
 var color_disabled = "#903738";
 
-function allow_next(next_button, enable, text_on_false){
-    if(text_on_false == null){
-        text_on_false = "Please answer the questions above";
-    } 
-    if(enable){
-        next_button.innerText = "Continue";
-        next_button.disabled = false;
-        next_button.style.backgroundColor = color_enabled;
+function allow_next(next, enable, prompt) {
+    if (prompt == null) {
+        prompt = "Please answer the questions above";
     }
-    if(!enable){
-        next_button.innerText = text_on_false;
-        next_button.disabled = true;  
-        next_button.style.backgroundColor = color_disabled;
+    if (enable) {
+        next.innerText = "Continue";
+        next.disabled = false;
+        next.style.backgroundColor = color_enabled;
+    }
+    if (!enable) {
+        next.innerText = prompt;
+        next.disabled = true;
+        next.style.backgroundColor = color_disabled;
         is_valid = false;
     }
 }
 
-function get_answer(questions, question){
+function get_answer(questions, question) {
     return questions[question].children[1].value;
 }
 
-function update_known_langs(questions){
+function update_known_langs(questions) {
     var known = [];
-    for(var i = 0; i<4; i++){
-        if(get_answer(questions, i+1) != ''){
-            known[i] = get_answer(questions, i+1);
-        }else{
+    for (var i = 0; i < 4; i++) {
+        if (get_answer(questions, i + 1) != '') {
+            known[i] = get_answer(questions, i + 1);
+        } else {
             break;
         }
     }
     langs = known;
 }
-function get_known_langs(){
+
+function get_known_langs() {
     return langs;
 }
-function validate_contact(){
+
+function validate_contact() {
     var questions = document.getElementById("jspsych-content").children;
-    var next_button = document.getElementById("jspsych-survey-text-next");
-    allow_next(next_button, false);
+    var next = document.getElementById("jspsych-survey-text-next");
+    allow_next(next, false);
     is_valid = true;
 
     var lname = get_answer(questions, 1);
@@ -52,27 +54,27 @@ function validate_contact(){
     //regex from: https://cs.chromium.org/chromium/src/third_party/WebKit/LayoutTests/fast/forms/resources/ValidityState-typeMismatch-email.js?sq=package:chromium&type=cs
     var email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if(is_valid && (lname.length == 0 || lname.length > 100)){
-        allow_next(next_button, false, "Enter your surname");
-    } 
-    if(is_valid && fname.length == 0 || lname.length > 100){
-        allow_next(next_button, false, "Enter your first name");
+    if (is_valid && (lname.length == 0 || lname.length > 100)) {
+        allow_next(next, false, "Enter your surname");
     }
-    if(is_valid && phone.length < 5 || phone.length > 100){
-        allow_next(next_button, false, "Enter a valid phone number");
-    } 
-    if(is_valid && !email_regex.test(String(email).toLowerCase())){
-        allow_next(next_button, false, "Enter a valid email address");
+    if (is_valid && fname.length == 0 || lname.length > 100) {
+        allow_next(next, false, "Enter your first name");
     }
-    if(is_valid){
-        allow_next(next_button, true);
-    }    
+    if (is_valid && phone.length < 5 || phone.length > 100) {
+        allow_next(next, false, "Enter a valid phone number");
+    }
+    if (is_valid && !email_regex.test(String(email).toLowerCase())) {
+        allow_next(next, false, "Enter a valid email address");
+    }
+    if (is_valid) {
+        allow_next(next, true);
+    }
 }
 
-function validate_personal(){
+function validate_personal() {
     var questions = document.getElementById("jspsych-content").children;
-    var next_button = document.getElementById("jspsych-survey-text-next");
-    allow_next(next_button, false);
+    var next = document.getElementById("jspsych-survey-text-next");
+    allow_next(next, false);
     is_valid = true;
 
     var age = get_answer(questions, 1);
@@ -81,30 +83,30 @@ function validate_personal(){
     var language_disorders = get_answer(questions, 4);
     var learning_disorders = get_answer(questions, 5);
 
-    if(is_valid && (isNaN(age) || age <= 0 || age > 120)){
-        allow_next(next_button, false, "Enter your age (0-120)");
+    if (is_valid && (isNaN(age) || age <= 0 || age > 120)) {
+        allow_next(next, false, "Enter your age (0-120)");
     }
-    if(is_valid && (gender.length == 0 || gender.length > 100)){
-        allow_next(next_button, false, "Enter your gender");
+    if (is_valid && (gender.length == 0 || gender.length > 100)) {
+        allow_next(next, false, "Enter your gender");
     }
-    if(is_valid && (hearing_problems.length > 1000)){
-        allow_next(next_button, false, "Describe any hearing problems in under 1000 characters");
+    if (is_valid && (hearing_problems.length > 1000)) {
+        allow_next(next, false, "Describe any hearing problems in under 1000 characters");
     }
-    if(is_valid && (language_disorders.length > 1000)){
-        allow_next(next_button, false, "Describe any language disorders in under 1000 characters");
+    if (is_valid && (language_disorders.length > 1000)) {
+        allow_next(next, false, "Describe any language disorders in under 1000 characters");
     }
-    if(is_valid && (learning_disorders.length > 1000)){
-        allow_next(next_button, false, "Describe any learning disorders in under 1000 characters");
+    if (is_valid && (learning_disorders.length > 1000)) {
+        allow_next(next, false, "Describe any learning disorders in under 1000 characters");
     }
-    if(is_valid){
-        allow_next(next_button, true);
+    if (is_valid) {
+        allow_next(next, true);
     }
 }
 
-function validate_background(){
+function validate_background() {
     var questions = document.getElementById("jspsych-content").children;
-    var next_button = document.getElementById("jspsych-survey-text-next");
-    allow_next(next_button, false);
+    var next = document.getElementById("jspsych-survey-text-next");
+    allow_next(next, false);
     is_valid = true;
 
     var birth_country = get_answer(questions, 1);
@@ -112,78 +114,80 @@ function validate_background(){
     var native_language = get_answer(questions, 3);
     var parent_1_lang = get_answer(questions, 4);
     var parent_2_lang = get_answer(questions, 5);
-    
-    
-    if(is_valid && (birth_country.length == 0 || birth_country.length > 100)){
-        allow_next(next_button, false, "Enter your country of birth");
-    }    
-    if(is_valid && (isNaN(parseInt(age_arrival)) || ( age_arrival < 0 || age_arrival > 120))){
-        allow_next(next_button, false, "Enter your age of arrival in Canada (0-120)");
+
+
+    if (is_valid && (birth_country.length == 0 || birth_country.length > 100)) {
+        allow_next(next, false, "Enter your country of birth");
     }
-    if(is_valid && (native_language.length < 2 || native_language.length > 50)){
-        allow_next(next_button, false, "Enter your native language");
+    if (is_valid && (isNaN(parseInt(age_arrival)) || (age_arrival < 0 || age_arrival > 120))) {
+        allow_next(next, false, "Enter your age of arrival in Canada (0-120)");
     }
-    if(is_valid && (parent_1_lang.length < 2 || parent_1_lang.length > 50)){
-        allow_next(next_button, false, "Enter Parent 1's native language");
+    if (is_valid && (native_language.length < 2 || native_language.length > 50)) {
+        allow_next(next, false, "Enter your native language");
     }
-    if(is_valid && (parent_2_lang.length < 2 || parent_2_lang.length > 50)){
-        allow_next(next_button, false, "Enter Parent 2's native language");
+    if (is_valid && (parent_1_lang.length < 2 || parent_1_lang.length > 50)) {
+        allow_next(next, false, "Enter Parent 1's native language");
     }
-    if(is_valid){
-        allow_next(next_button, true);
+    if (is_valid && (parent_2_lang.length < 2 || parent_2_lang.length > 50)) {
+        allow_next(next, false, "Enter Parent 2's native language");
+    }
+    if (is_valid) {
+        allow_next(next, true);
     }
 }
-function validate_dominant_languages(){
+
+function validate_dominant_languages() {
     var questions = document.getElementById("jspsych-content").children;
-    var next_button = document.getElementById("jspsych-survey-text-next");
-    allow_next(next_button, false);
+    var next = document.getElementById("jspsych-survey-text-next");
+    allow_next(next, false);
     is_valid = true;
-    
+
     var dom_lang1 = get_answer(questions, 1);
 
-    if (dom_lang1.length == 0 || dom_lang1.length > 50){
-        allow_next(next_button, false, "Enter at least your most dominant language");
+    if (dom_lang1.length == 0 || dom_lang1.length > 50) {
+        allow_next(next, false, "Enter at least your most dominant language");
     }
 
-    if(is_valid){
+    if (is_valid) {
         update_known_langs(questions);
-        allow_next(next_button, true);
+        allow_next(next, true);
     }
 }
 
-function validate_language_details(){
-    var next_button = document.getElementById("jspsych-language-info-next");
-    allow_next(next_button, false);
+function validate_language_details() {
+    var next = document.getElementById("jspsych-language-info-next");
+    allow_next(next, false);
     is_valid = true;
-    for(var i=0; i<4; i++){
+    for (var i = 0; i < 4; i++) {
         currRow = document.getElementById("lang-row-" + i);
-        if(currRow == null || !is_valid){
+        if (currRow == null || !is_valid) {
             break;
         }
         var age_began = currRow.children[1].children[0].value;
         var years_learned = currRow.children[2].children[0].value;
-        if (isNaN(parseInt(age_began)) || ( age_began < 0 || age_began > 120)){
-            allow_next(next_button, false, "Please enter age you began learning "+ currRow.children[0].innerText);
-        } else if (isNaN(parseInt(years_learned)) || (years_learned < 0 || years_learned > 120)){
-            allow_next(next_button, false, "Please specify how many years you have learned " + currRow.children[0].innerText);
+        if (isNaN(parseInt(age_began)) || (age_began < 0 || age_began > 120)) {
+            allow_next(next, false, "Please enter age you began learning " + currRow.children[0].innerText);
+        } else if (isNaN(parseInt(years_learned)) || (years_learned < 0 || years_learned > 120)) {
+            allow_next(next, false, "Please specify how many years you have learned " + currRow.children[0].innerText);
         }
     }
-    if(is_valid){
-        allow_next(next_button, true);
+    if (is_valid) {
+        allow_next(next, true);
     }
 }
-function validate_musical(){
+
+function validate_musical() {
     var questions = document.getElementById("jspsych-content").children;
-    var next_button = document.getElementById("jspsych-survey-text-next");
-    allow_next(next_button, false);
+    var next = document.getElementById("jspsych-survey-text-next");
+    allow_next(next, false);
     is_valid = true;
 
     var musical_instruments = get_answer(questions, 1);
 
-    if(is_valid && (musical_instruments.length > 1000)){
-        allow_next(next_button, false, "Describe any musical experience in under 1000 characters");
+    if (is_valid && (musical_instruments.length > 1000)) {
+        allow_next(next, false, "Describe any musical experience in under 1000 characters");
     }
-    if(is_valid){
-        allow_next(next_button, true);
+    if (is_valid) {
+        allow_next(next, true);
     }
 }
