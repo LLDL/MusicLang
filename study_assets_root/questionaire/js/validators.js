a16f50cd47cdcaf7cfde3b2e89ea989144edec30
adassor <adassor@sfu.ca>
2018-05-18 11:35:46 -0700

    simplified possible states for validation: less error prone

--- study_assets_root/questionaire/js/validators.js
+++ study_assets_root/questionaire/js/validators.js
@@ -1,4 +1,3 @@
-var is_valid;
 var known_langs;
 var color_enabled = "#37904b";
 var color_disabled = "#903738";
@@ -6,11 +5,11 @@ var color_disabled = "#903738";
 function toggle_listeners(form, on, validator){
     if(on){
         form.addEventListener("change", validator);
-        // form.addEventListener("mousemove", validator);
+        form.addEventListener("mousemove", validator);
         form.addEventListener("keyup", validator);
     }else{
         form.removeEventListener("change", validator);
-        // form.removeEventListener("mousemove", validator);
+        form.removeEventListener("mousemove", validator);
         form.removeEventListener("keyup", validator);
     }
 
@@ -29,7 +28,6 @@ function allow_next(next, enable, prompt) {
         next.innerText = prompt;
         next.disabled = true;
         next.style.backgroundColor = color_disabled;
-        is_valid = false;
     }
 }
 
@@ -56,8 +54,8 @@ function get_known_langs() {
 function validate_contact() {
     var questions = document.getElementById("jspsych-content").children;
     var next = document.getElementById("jspsych-survey-text-next");
+    //start with next disabled
     allow_next(next, false);
-    is_valid = true;
 
     var lname = get_answer(questions, 1);
     var fname = get_answer(questions, 2);
@@ -67,19 +65,15 @@ function validate_contact() {
     //regex from: https://cs.chromium.org/chromium/src/third_party/WebKit/LayoutTests/fast/forms/resources/ValidityState-typeMismatch-email.js?sq=package:chromium&type=cs
     var email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (is_valid && (lname.length == 0 || lname.length > 100)) {
+    if ((lname.length == 0 || lname.length > 100)) {
         allow_next(next, false, "Enter your surname");
-    }
-    if (is_valid && fname.length == 0 || lname.length > 100) {
+    } else if (fname.length == 0 || lname.length > 100) {
         allow_next(next, false, "Enter your first name");
-    }
-    if (is_valid && phone.length < 5 || phone.length > 100) {
+    } else if (phone.length < 5 || phone.length > 100) {
         allow_next(next, false, "Enter a valid phone number");
-    }
-    if (is_valid && !email_regex.test(String(email).toLowerCase())) {
+    } else if (!email_regex.test(String(email).toLowerCase())) {
         allow_next(next, false, "Enter a valid email address");
-    }
-    if (is_valid) {
+    } else {
         allow_next(next, true);
     }
 }
@@ -88,37 +82,41 @@ function validate_personal() {
     var questions = document.getElementById("jspsych-content").children;
     var next = document.getElementById("jspsych-survey-text-next");
     allow_next(next, false);
-    is_valid = true;
+
 
     var age = get_answer(questions, 1);
     var hearing_problems = get_answer(questions, 2);
     var language_disorders = get_answer(questions, 3);
     var learning_disorders = get_answer(questions, 4);
 
-    if (is_valid && (isNaN(age) || age <= 0 || age > 120)) {
+    if (isNaN(age) || age <= 0 || age > 120) {
         allow_next(next, false, "Enter your age (0-120)");
-    }
-    if (is_valid && (hearing_problems.length > 1000)) {
+    } else if (hearing_problems.length > 1000) {
         allow_next(next, false, "Describe any hearing problems in under 1000 characters");
-    }
-    if (is_valid && (language_disorders.length > 1000)) {
+    } else if (language_disorders.length > 1000) {
         allow_next(next, false, "Describe any language disorders in under 1000 characters");
-    }
-    if (is_valid && (learning_disorders.length > 1000)) {
+    } else if (learning_disorders.length > 1000) {
         allow_next(next, false, "Describe any learning disorders in under 1000 characters");
-    }
-    if (is_valid) {
+    } else {
         allow_next(next, true);
     }
 }
 
 function validate_gender() {
     var next = document.getElementById("jspsych-gender-info-next");
-    allow_next(next, false);
-    is_valid = true;
+    allow_next(next, false, "Select or enter your gender");
 
-
-    if (is_valid) {
+    var gender;
+    if(document.getElementById("gender-female").checked){
+        gender = "female";
+        allow_next(next, true);
+    }else if(document.getElementById("gender-male").checked){
+        gender = "male";
+        allow_next(next, true);
+    }else if(document.getElementById("gender-other").checked && document.getElementById("gender-specified").value == ''){
+        is_valid = false;
+    }else if(document.getElementById("gender-other").checked){
+        gender = document.getElementById("gender-specified").value;
         allow_next(next, true);
     }
 }
@@ -127,7 +125,6 @@ function validate_background() {
     var questions = document.getElementById("jspsych-content").children;
     var next = document.getElementById("jspsych-survey-text-next");
     allow_next(next, false);
-    is_valid = true;
 
     var birth_country = get_answer(questions, 1);
     var age_arrival = get_answer(questions, 2);
@@ -136,22 +133,17 @@ function validate_background() {
     var parent_2_lang = get_answer(questions, 5);
 
 
-    if (is_valid && (birth_country.length == 0 || birth_country.length > 100)) {
+    if (birth_country.length == 0 || birth_country.length > 100) {
         allow_next(next, false, "Enter your country of birth");
-    }
-    if (is_valid && (isNaN(parseInt(age_arrival)) || (age_arrival < 0 || age_arrival > 120))) {
+    } else if (isNaN(parseInt(age_arrival)) || (age_arrival < 0 || age_arrival > 120)) {
         allow_next(next, false, "Enter your age of arrival in Canada (0-120)");
-    }
-    if (is_valid && (native_language.length < 2 || native_language.length > 50)) {
+    } else if ((native_language.length < 2 || native_language.length > 50)) {
         allow_next(next, false, "Enter your native language");
-    }
-    if (is_valid && (parent_1_lang.length < 2 || parent_1_lang.length > 50)) {
+    } else if ((parent_1_lang.length < 2 || parent_1_lang.length > 50)) {
         allow_next(next, false, "Enter Parent 1's native language");
-    }
-    if (is_valid && (parent_2_lang.length < 2 || parent_2_lang.length > 50)) {
+    } else if (parent_2_lang.length < 2 || parent_2_lang.length > 50) {
         allow_next(next, false, "Enter Parent 2's native language");
-    }
-    if (is_valid) {
+    } else {
         allow_next(next, true);
     }
 }
@@ -160,15 +152,12 @@ function validate_dominant_languages() {
     var questions = document.getElementById("jspsych-content").children;
     var next = document.getElementById("jspsych-survey-text-next");
     allow_next(next, false);
-    is_valid = true;
 
     var dom_lang1 = get_answer(questions, 1);
 
     if (dom_lang1.length == 0 || dom_lang1.length > 50) {
         allow_next(next, false, "Enter at least your most dominant language");
-    }
-
-    if (is_valid) {
+    } else {
         update_known_langs(questions);
         allow_next(next, true);
     }
@@ -177,7 +166,7 @@ function validate_dominant_languages() {
 function validate_language_details() {
     var next = document.getElementById("jspsych-language-info-next");
     allow_next(next, false);
-    is_valid = true;
+    var is_valid = true;
     for (var i = 0; i < 5; i++) {
         currRow = document.getElementById("lang-row-" + i);
         if (currRow == null || !is_valid) {
@@ -187,8 +176,10 @@ function validate_language_details() {
         var years_learned = currRow.children[2].children[0].value;
         if (isNaN(parseInt(age_began)) || (age_began < 0 || age_began > 120)) {
             allow_next(next, false, "Please enter age you began learning " + currRow.children[0].innerText);
+            is_valid = false;
         } else if (isNaN(parseInt(years_learned)) || (years_learned < 0 || years_learned > 120)) {
             allow_next(next, false, "Please specify how many years you have learned " + currRow.children[0].innerText);
+            is_valid = false;
         }
     }
     if (is_valid) {
@@ -200,14 +191,12 @@ function validate_musical() {
     var questions = document.getElementById("jspsych-content").children;
     var next = document.getElementById("jspsych-survey-text-next");
     allow_next(next, false);
-    is_valid = true;
 
     var musical_instruments = get_answer(questions, 1);
 
     if (is_valid && (musical_instruments.length > 1000)) {
         allow_next(next, false, "Describe any musical experience in under 1000 characters");
-    }
-    if (is_valid) {
+    } else {
         allow_next(next, true);
     }
 }
