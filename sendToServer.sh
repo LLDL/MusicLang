8b2f73432021111ad508c1cbc25fede87e494b48
adassor <adassor@sfu.ca>
2018-07-06 09:32:12 -0700

    + updated server script + bugfixes for html

--- sendToServer.sh
+++ sendToServer.sh
@@ -1,17 +1,21 @@
 echo 'Generating tarball'
-cd study_assets_root
-tar -zcvf questionaire.tgz questionaire
+tar -zcvf study_assets_root.tgz study_assets_root
 
 echo 'Sending tarball'
-scp questionaire.tgz langdev@167.99.177.178:~/MusicLang/study_assets_root/questionaire.tgz
+scp study_assets_root.tgz langdev@167.99.177.178:~/MusicLang/study_assets_root.tgz
 
 echo 'SSH'
 ssh langdev@167.99.177.178 '
-cd ~/MusicLang/study_assets_root
-rm -r questionaire
-tar -xvzf questionaire.tgz
-rm questionaire.tgz
+cd ~/MusicLang/
+rm -r study_assets_root
+tar -xvzf study_assets_root.tgz
+rm study_assets_root.tgz
+rm ~/MusicLang/study_assets_root/musiclang_engnat/shared_assets
+ln -s ~/MusicLang/study_assets_root/shared_assets ~/MusicLang/study_assets_root/musiclang_engnat/shared_assets
+rm ~/MusicLang/study_assets_root/musiclang_mannat/shared_assets
+ln -s ~/MusicLang/study_assets_root/shared_assets ~/MusicLang/study_assets_root/musiclang_mannat/shared_assets
+
 '
 
 echo 'Removing local tarball'
-rm questionaire.tgz
+rm study_assets_root.tgz
