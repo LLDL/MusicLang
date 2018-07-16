740a007054e2eadd1cb11dbc2e1d6610ef278c52
adassor <adassor@sfu.ca>
2018-07-16 14:16:18 -0700

    audio normalized

--- study_assets_root/musiclang_mannat/man-nat.js
+++ study_assets_root/musiclang_mannat/man-nat.js
@@ -13,15 +13,6 @@ var gen_inst = {
     allow_keys: false
 };
 
-// var autoplay_inst = {
-//     type: 'instructions',
-//     pages: [
-//         header + '<h2>Audio Check</h2><p>If you can currently hear an audio clip, scroll to the bottom of the page and press Next. If you cannot currently hear an audio clip, your web browser may be blocking auto-playing audio. Please see the instructions below to enable auto-playing audio and do not press Next until you can hear the audio clip.</p> <h3>Google Chrome, Mozille Firefox, or Microsoft Edge</h3><p>Right click the current tab (LangDev MusicLang at the top of your browser) and press "Unmute Site". You should now hear audio and continue.</p><h3>Safari</h3><p>On the current tab (LangDev MusicLang at the top of your browser), there will be a sound icon to its right. Press the sound icon to unmute. You should now hear audio and continue.</p><audio preload="auto" autoplay loop><source src="sample.mp3" type="audio/mpeg"></audio>'
-//     ],
-//     show_clickable_nav: true,
-//     button_label_next: 'Next',
-//     allow_keys: false
-// };
 
 var inst_met_rhy_en = {
     type: 'instructions',
