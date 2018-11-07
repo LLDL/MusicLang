ebdff66c538b754c0e84e2ff5b34e1e2a0d3876b
adassor <adassor@sfu.ca>
2018-11-07 13:58:11 -0800

    added separate debrief experiments

---
+++ study_assets_root/musiclang_mannat/debrief.js
@@ -0,0 +1,31 @@
+
+var header = "<img id=\"logo\" src=\"shared_assets/img/langdev-logo.jpg\"</img><h1>Thank You</h1>"; //to be prepended to preludes
+
+jatos.onLoad(function() {
+    var finish_conf = {
+        type: 'instructions',
+        pages: [
+            header + '<h2>Study Complete</h2><p>Thank you for your participation. Your confirmation ID is <b>MLM' + jatos.studyResultId + '</b>. To arrange payment, please email <a href="mailto:langdev@sfu.ca?Subject=Participant%20MLM' + jatos.studyResultId + '">langdev@sfu.ca</a> with the subject <b>Participant MLM' + jatos.studyResultId + '</b></p>'
+        ],
+        show_clickable_nav: true,
+        button_label_next: 'Close',
+        allow_keys: false
+    };
+    jsPsych.init({
+        //Questionaire:
+        timeline: [finish_conf],
+        
+        exclusions: {
+            min_width: 800,
+            min_height: 600
+        },
+        on_finish: function (data) {
+            var studyID = jatos.studyResultId;
+            jsPsych.data.addProperties({subject : studyID});
+            var resultsRaw = jsPsych.data.get();
+            var results = resultsRaw.ignore('internal_node_id').ignore('time_elapsed');
+            var resultsCSV = results.csv();
+            jatos.submitResultData(resultsCSV, jatos.startNextComponent);
+        }
+    })
+});
--- study_assets_root/musiclang_mannat/man-nat.js
+++ study_assets_root/musiclang_mannat/man-nat.js
@@ -254,17 +254,8 @@ var ppc_for_mand = {
 
 
 jatos.onLoad(function() {
-    var finish_conf = {
-        type: 'instructions',
-        pages: [
-            header + '<h2>Study Complete</h2><p>Thank you for your participation. Your confirmation ID is <b>M' + jatos.studyResultId + '</b>. To arrange payment, please email <a href="mailto:langdev@sfu.ca?Subject=MusicLang%20Participant%20M' + jatos.studyResultId + '">langdev@sfu.ca</a> with the subject <b>MusicLang Participant M' + jatos.studyResultId + '</b></p>'
-        ],
-        show_clickable_nav: true,
-        button_label_next: 'Close',
-        allow_keys: false
-    };
     var counterBalance1 = {
-        timeline: [gen_inst, inst_met_rhy_for_mand, met_rhy_for_mand, inst_met_mel_for_mand, met_mel_for_mand, inst_rpcv_for_mand, rpcv_for_mand,inst_rpst_for_mand, rpst_for_mand, lk_for_mand, ppc_for_mand, finish_conf],
+        timeline: [gen_inst, inst_met_rhy_for_mand, met_rhy_for_mand, inst_met_mel_for_mand, met_mel_for_mand, inst_rpcv_for_mand, rpcv_for_mand,inst_rpst_for_mand, rpst_for_mand, lk_for_mand, ppc_for_mand],
         conditional_function: function(){
             if(jatos.studyResultId % 2 == 1){
                 // console.log("odd");
@@ -276,7 +267,7 @@ jatos.onLoad(function() {
     };
     
     var counterBalance2 = {
-        timeline: [gen_inst,inst_met_mel_for_mand, met_mel_for_mand, inst_met_rhy_for_mand, met_rhy_for_mand, inst_rpst_for_mand, rpst_for_mand,inst_rpst_for_mand, rpcv_for_mand, lk_for_mand, ppc_for_mand, finish_conf], 
+        timeline: [gen_inst,inst_met_mel_for_mand, met_mel_for_mand, inst_met_rhy_for_mand, met_rhy_for_mand, inst_rpst_for_mand, rpst_for_mand,inst_rpst_for_mand, rpcv_for_mand, lk_for_mand, ppc_for_mand], 
         conditional_function: function(){
             if(jatos.studyResultId % 2 == 0){
                 // console.log("odd");
