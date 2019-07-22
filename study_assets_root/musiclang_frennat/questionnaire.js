032746552dfe222cee6e446601df5f963fda3452
LangDevLab <38764712+LangDevLab@users.noreply.github.com>
2019-07-22 14:50:58 -0700

    Update questionnaire.js

--- study_assets_root/musiclang_frennat/questionnaire.js
+++ study_assets_root/musiclang_frennat/questionnaire.js
@@ -9,7 +9,7 @@ var info = {
     type: 'instructions',
     pages: [
         //header + '<h2>Participant Consent</h2><p>Please download and read <a href="shared_assets/consent.pdf" target="_blank">this consent form</a>. By continuing, you are consenting to our study.</p> '
-        '<h2>Participant Consent</h2><p>Please download and read <a href="shared_assets/consent.pdf" target="_blank">this consent form</a>. By continuing, you are consenting to our study.</p> '
+        '<h2>Participant Consent</h2><p>Please download and read <a href="shared_assets/consent-frennat.pdf" target="_blank">this consent form</a>. By continuing, you are consenting to our study.</p> '
     ],
     show_clickable_nav: true,
     button_label_next: 'Next',
