0e99f23643287bd42e1f9107623dc103a35daf21
LangDevLab <langdev@sfu.ca>
2020-07-04 00:38:54 -0700

    combined debrief & RPS
    combined the debrief & RPS pages:
    After they have completed the experiment, participants will see an end screen with their confirmation ID.
    If they are completing for cash, they are told to email us their confirmation ID to arrange payment. After they have sent the email, they are told to press "N" to close the experiment.
    If they are completing for RPS, they are told to press "Y" to complete the online RPS form.

---
+++ study_assets_root/RespOnline/EndScreen.js
@@ -0,0 +1,109 @@
+var blur_count = 0; //number of times subject's focus leaves tab
+var form; //current jspych-content element, to allow listeners to be toggled from different functions
+var likely_invalid = false; //gets set to true if blur_count>threshold
+
+var header = "<img id=\"logo\" src=\"shared_assets/img/langdev-logo.jpg\"</img><h1>Language Learning & Development Lab RespOnline</h1>"; //to be prepended to preludes
+
+// Instruction pages that appear at the start
+
+jatos.onLoad(function() {
+
+var cash_rps = {
+type: 'html-keyboard-response',
+stimulus: header + '<h2>Study Complete</h2><p>Thank you for your participation. Your confirmation ID is <b>RO' + jatos.studyResultId + '</b>. <p> If you have completed this study for cash, please email <a href="mailto:langdev@sfu.ca?Subject=Participant%20MLE' + jatos.studyResultId + '">langdev@sfu.ca</a> with the subject <b>Participant RO' + jatos.studyResultId + '</b>. to arrange payment.' +
+' After you have emailed us your confirmation ID, you may press <b> N </b> to end the experiment.</p>' +
+'<p>If you have completed this study for RPS credits, press <b> Y </b> to complete the online RPS form. </p>',
+choices: [89,78], // Y or N
+on_finish: function(data){
+if(data.key_press == 78){
+  jsPsych.endExperiment('The experiment was ended.');
+}
+}
+}
+
+var online = {
+    type: 'instructions',
+    pages: [
+        header + '<h2>Online RPS Form</h2>'
+    ],
+    show_clickable_nav: true,
+    button_label_next: 'Next',
+    button_label_previous: 'Back',
+    allow_keys: false
+}
+
+var confirmation = {
+    type: 'survey-yes-no',
+    questions: [
+        {prompt: "Have you taken this study in the past for RPS credits?" +
+      "<p>If you have already taken this study in the past for course credits, you may not use this study for RPS again.</p>"}
+    ],
+    preamble: header + "<h2>Confirmation</h2>",
+    json_label: 'complete_RPS_past',
+    on_load: function(){
+        validate_confirmation();
+        form = document.getElementById("jspsych-content");
+        toggle_listeners(form, true, validate_confirmation);
+    },
+    on_finish: function(){
+        toggle_listeners(form, false, validate_confirmation);
+    }
+}
+/*
+    General Form of a Trial's nontrivial sections:
+    on_load: function(){...}
+        1. Tries and fails to validate the form, disabling next
+        2. Sets the value of 'form' and uses that as a jumping off point to toggle form change listeners
+    on_finish: function(){...}
+        1   releases listeners
+*/
+var rps = {
+    type: 'survey-text-custom',
+    preamble: header + "<h2>RPS Information</h2>",
+    json_label: 'RPS Information',
+    questions: [
+        {prompt: "Full Name"},
+        {prompt: "Student Number"},
+        {prompt: "Course for RPS credit <br>e.g. LING 200</br>"},
+        {prompt: "Semester and Year <br> Summer 2020</br>"},
+        {prompt: "Course Instructor"},
+        {prompt: "Notes (e.g. if you wish to divide RPS credits among different courses, please confirm and list the courses here)"}
+    ],
+    on_load: function () {
+        validate_rps();
+        form = document.getElementById("jspsych-content");
+        toggle_listeners(form, true, validate_rps);
+    },
+    on_finish: function () {
+        toggle_listeners(form, false, validate_rps);
+    }
+
+  };
+
+      var finish_conf = {
+          type: 'instructions',
+          pages: [
+              header + '<h2>Study Complete</h2><p>Thank you for your participation. Your confirmation ID is <b>' + jatos.studyResultId + '</b>.' +
+              '<p> Your participation is counted in the RPS system.</p>' +
+              '<p> If you have any questions about your participation, please save this ID and email <a href="mailto:langdev@sfu.ca">langdev@sfu.ca</a>.</p>'
+            ],
+          show_clickable_nav: true,
+          button_label_next: 'Close',
+          allow_keys: false
+      };
+      jsPsych.init({
+        timeline: [cash_rps, online, confirmation, rps, finish_conf],
+        exclusions: {
+            min_width: 800,
+            min_height: 600
+        },
+        on_finish: function (data) {
+            var studyID = jatos.studyResultId;
+            jsPsych.data.addProperties({subject : studyID});
+            var resultsRaw = jsPsych.data.get();
+            var results = resultsRaw.ignore('internal_node_id').ignore('time_elapsed');
+            var resultsCSV = results.csv();
+            jatos.submitResultData(resultsCSV, jatos.startNextComponent);
+          }
+      })
+  });
